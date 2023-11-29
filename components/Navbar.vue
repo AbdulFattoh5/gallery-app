22e4fd07aac8de7723e73e58211a3e53fc0f6e73
Abdulfattoh <abdulfattokh55@gmail.com>
2023-11-29 12:51:41 +0500

    added input to header

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,23 +1,27 @@
 <template>
   <header class="header">
     <div class="container">
-        <div class="header__wrap">
-            <div class="header__logo">
-                <img src="@/assets/img/icon.png" alt="" class="header__logo-img">
-                <h1 class="header__logo-title">Gallery</h1>
-            </div>
-            <button class="header__btn"><img src="@/assets/img/load-more-icon.png" alt="">Load</button>
+      <div class="header__wrap">
+        <div class="header__logo">
+          <img src="@/assets/img/icon.png" alt="" class="header__logo-img" />
+          <h1 class="header__logo-title">Gallery</h1>
         </div>
+        <button class="header__btn">
+          <img src="@/assets/img/load-more-icon.png" alt="" />Load
+        </button>
+        <div class="header__input">
+          <input type="text" class="header__input-text" placeholder="Search images">
+          <button class="header__input-btn"><img src="@/assets/img/search-icon.png" alt=""></button>
+        </div>
+      </div>
     </div>
   </header>
 </template>
 
 <script>
 export default {
-    name: "Navbar",
-}
+  name: "Navbar",
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
