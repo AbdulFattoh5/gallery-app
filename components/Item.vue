eab38f2ea9a0638a3c2096bc45337820a1762383
Abdulfattoh <abdulfattokh55@gmail.com>
2023-11-29 13:07:36 +0500

    item created

---
+++ components/Item.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="item">
+    <img src="@/assets/img/def.jpg" alt="" class="item__img">
+    <div class="item__author">
+        <img src="@/assets/img/author.png" alt="" class="item__author-icon">
+        <h3 class="item__author-name">James Bond</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
