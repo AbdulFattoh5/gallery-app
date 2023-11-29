4fef7e16398caf428cf2574ce8b8dfe3b60f74ad
Abdulfattoh <abdulfattokh55@gmail.com>
2023-11-29 13:18:50 +0500

    items component added

---
+++ components/Items.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="items">
+    <div class="container">
+        <div class="items__wrap">
+            <Item />
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Item from './Item.vue'
+export default {
+    name: "Items",
+    components:{
+        Item,
+    }
+}
+</script>
+
+<style>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
     <Navbar />
+    <Items />
   </div>
 </template>
 
 <script>
+import Items from '../components/Items.vue';
 import Navbar from '../components/Navbar.vue';
 export default {
   name: "IndexPage",
   components:{
     Navbar,
+    Items,
   }
 };
 </script>
