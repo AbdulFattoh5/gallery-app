e7ff7363e33766c3d56c304f3f14d87d8340347a
Abdulfattoh <abdulfattokh55@gmail.com>
2023-11-29 12:17:00 +0500

    header

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,6 +1,14 @@
 <template>
   <header class="header">
-
+    <div class="container">
+        <div class="header__wrap">
+            <div class="header__logo">
+                <img src="@/assets/img/icon.png" alt="" class="header__logo-img">
+                <h1 class="header__logo-title">Gallery</h1>
+            </div>
+            <button class="header__btn"><img src="@/assets/img/load-more-icon.png" alt="">Load</button>
+        </div>
+    </div>
   </header>
 </template>
 
