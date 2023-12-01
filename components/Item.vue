4a75b7ef2586de8987ed60549c4681f4342142e6
Abdulfattoh <abdulfattokh55@gmail.com>
2023-12-01 14:20:57 +0500

    added download function

--- components/Item.vue
+++ components/Item.vue
@@ -2,7 +2,10 @@
   <div class="item">
     <img :src="photo?.src?.large" alt="" class="item__img" />
     <div class="item__details">
-      <a download :href="photo?.src?.large" target="_blank"
+      <a
+        @click.prevent="downloadImage"
+        :href="photo?.src?.large"
+        target="_blank"
         ><img src="@/assets/img/enter-icon.png" alt="" class="item__link"
       /></a>
       <div class="item__author">
@@ -14,12 +17,38 @@
 </template>
 
 <script>
+import axios from "axios"
 export default {
   data() {
     return {};
   },
   props: {
     photo: Object,
+  },
+  methods: {
+    async downloadImage() {
+      try {
+        const response = await axios.get(this.photo?.src?.large, {
+          responseType: "blob",
+        });
+        const blob = new Blob([response.data]);
+        const url = window.URL.createObjectURL(blob);
+
+        // Create a download link
+        const link = document.createElement("a");
+        link.href = url;
+        link.download = "downloaded_gallery.jpg";
+
+        // Append the link to the body and trigger the click event
+        document.body.appendChild(link);
+        link.click();
+
+        // Remove the link from the DOM
+        document.body.removeChild(link);
+      } catch (error) {
+        console.error("Error downloading image:", error);
+      }
+    },
   },
 };
 </script>
